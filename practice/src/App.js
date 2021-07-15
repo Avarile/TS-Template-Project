73e32692a79b04bedb891bda868da187d659c1bb
Avarile <avarile@gmail.com>
2021-07-15 06:10:23 -0400

    commit all

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  
+
   return (
-    <div className="App">
+    <>
       
-    </div>
+    </>
   );
 }
 
---
+++ practice/src/Components/FriendStatus.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function FriendStatus(props) {
+
+  const [ isOnline, setIsOnline ] = useState(null);
+
+  function handleStatusChange(){
+    setIsOnline()
+  }
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
